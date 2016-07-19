8ee2835b7379a8b8111c2e0807e8a11899fd5647
Ricky Avina <rickyavina@icloud.com>
2016-07-19 12:35:02 -0700

    Next Commit

--- Xpress/ViewControllerLoginPage.swift
+++ Xpress/ViewControllerLoginPage.swift
@@ -2,28 +2,20 @@
 //  ViewControllerLoginPage.swift
 //  Xpress
 //
-//  Created by Maricela Avina on 7/18/16.
+//  Created by Maricela Avina on 7/19/16.
 //  Copyright © 2016 RickyAvina. All rights reserved.
 //
 
 import UIKit
 
 class ViewControllerLoginPage: UIViewController {
-    
-    @IBOutlet var emailTextField: UITextField!
-    @IBOutlet var passwordTextField: UITextField!
-    
-    @IBAction func pushNewViewController(sender: AnyObject) {
-        
-    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
+    
 }
